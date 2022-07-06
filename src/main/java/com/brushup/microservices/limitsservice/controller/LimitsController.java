47c17014c796aba5e52d3750c3f980a2f386165a
chinthaka <chinthakarukshan@gmail.com>
2022-07-07 00:33:20 +0530

    Enhanced service to get configuration from property file

--- src/main/java/com/brushup/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/brushup/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.brushup.microservices.limitsservice.controller;
 
+import com.brushup.microservices.limitsservice.configuration.Configuration;
 import com.brushup.microservices.limitsservice.dto.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits getLimits () {
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 }
